9501ab91d74130c09f8b9c1af222b0be152682f0
Danilo Couto <couto.danilo95@gmail.com>
2022-06-29 12:45:44 -0300

    feat controlers

---
+++ src/controllers/task.ts
@@ -0,0 +1,44 @@
+import prismaClient from "../dataBase/prismaClient"
+import { Request, Response } from 'express';
+
+export class TaskController {
+    async create(req: Request, res: Response){
+        const { name, content } = req.body;
+
+        const task = await prismaClient.task.create({
+           data: {name, content}
+        });
+        return res.json(task);
+    }
+
+    async createWithUser(req: Request, res: Response){
+        const { name, content, id_user} = req.body;
+        console.log(req.body)
+
+        const task = await prismaClient.taskUser.create({
+            data: { 
+                task: {
+                    create: { name, content }
+                },
+            user: {
+                connect: { id: id_user }
+             }
+            }
+        });
+        return res.json(task);
+    }
+
+    async findAll(_req: Request, res: Response){
+        const tasks = await prismaClient.task.findMany()
+        return res.json(tasks);
+    }
+
+    async findById(req: Request, res: Response){
+        const { id } = req.params;
+        const product = await prismaClient.task.findUnique({
+            where: {id: +id},
+            include: {TaskUser: true}
+        })
+        return res.json(product);
+    }
+}
