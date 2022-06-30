bb5b04ee64a999caa59c7423d2b1589e027fb75c
Danilo Couto <couto.danilo95@gmail.com>
2022-06-30 12:07:53 -0300

    install but not ue queryclient

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -9,7 +9,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <QueryClientProvider client = { queryClient} > */}
+      <App />
+    {/* </QueryClientProvider> */}
   </React.StrictMode>
 );
 
