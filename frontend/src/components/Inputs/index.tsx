ae653a32c7d9e975b72da52a486e78016a815220
Danilo Couto <couto.danilo95@gmail.com>
2022-06-30 12:09:53 -0300

    Inputs componetization

---
+++ frontend/src/components/Inputs/index.tsx
@@ -0,0 +1,33 @@
+interface InputsProps {
+  title: string; 
+  setTitle: Function;
+  content: string;
+  setContent: Function;
+  owner: string; 
+  setOwner: Function;
+}
+
+export function Inputs({
+  title, setTitle,
+  content, setContent,
+  owner, setOwner
+}: InputsProps) {
+
+  const addTitle = ({target}: any) => setTitle(target.value);
+  const addContent = ({target}: any) => setContent(target.value);
+  const addOwner = ({target}: any) => setOwner(target.value === 'Danilo' ? '0' : '1'); 
+
+  return (
+    <>
+      <input type="text" required placeholder="Adicione uma tarefa" onChange={addTitle} value={title} />
+      <input type="text" required placeholder="Descreva a tarefa" onChange={addContent} value={content} />
+      <label> Escolha o responsável:
+        <select onChange={addOwner} required value={owner}>
+          <option value="Danilo">Danilo</option>
+          <option value="Murilo">Murilo</option>
+        </select>
+      </label>
+      <button type="submit">Adicionar</button>
+    </>
+  );
+}
