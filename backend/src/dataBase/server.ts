c01891ac0d285ff1ef3e042c6ba2fd03f17133ad
Danilo Couto <couto.danilo95@gmail.com>
2022-06-29 21:57:28 -0300

    ad use cors to server.ts

--- backend/src/dataBase/server.ts
+++ backend/src/dataBase/server.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import productRouter from './routes';
+const cors = require('cors')
 
 const app = express();
 
 app.use(express.json());
+app.use(cors())
+
 app.use(productRouter);
 
 app.use('/test', (req, res) => { res.json('Hello world') });
