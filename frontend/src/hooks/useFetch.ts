784802ef1a15cdde4a4ef7f04a5c846181feb49c
Danilo Couto <couto.danilo95@gmail.com>
2022-06-30 12:10:54 -0300

    rename useFetch function

--- frontend/src/hooks/useFetch.ts
+++ frontend/src/hooks/useFetch.ts
@@ -1,11 +1,12 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-export function useFeth<Response = unknown>(url: string){
+export function useFetch<Response = unknown>(url: string){
     const [taskList, setTaskList] = useState<Response | null[]>([]);
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState<Error | null>(null)
 
+    // buscar sobre stale while revalidation
     useEffect(()=> {
         axios.get(url).then(res => {
             setTaskList(res.data)
